881cfb3563e442b12542b7bd6a315bd452512d1d
Nibin Benny <nibin.benny@mwttech.com>
2024-01-12 12:10:09 +0530

    navbar designing in progress

---
+++ src/Components/Assets/all_product.js
@@ -0,0 +1,183 @@
+import p1_img from "./1.jpeg";
+import p2_img from "./2.jpeg";
+import p3_img from "./3.jpeg";
+import p4_img from "./4.jpeg";
+import p5_img from "./5.jpeg";
+import p6_img from "./6.jpeg";
+import p7_img from "./7.jpeg";
+import p8_img from "./8.jpeg";
+import p9_img from "./9.jpeg";
+import p10_img from "./10.jpeg";
+import p11_img from "./11.jpeg";
+import p12_img from "./12.jpeg";
+import p13_img from "./13.jpeg";
+import p14_img from "./14.jpeg";
+import p15_img from "./15.jpeg";
+import p16_img from "./16.jpeg";
+import p17_img from "./17.jpeg";
+import p18_img from "./18.jpeg";
+import p19_img from "./19.jpeg";
+import p20_img from "./20.jpg";
+
+let all_product=[
+    {
+        id:1,
+        name:"Full Sleeve Tshirts",
+        category:"women",
+        image:p1_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:2,
+        name:"Full Sleeve Tshirts",
+        category:"women",
+        image:p2_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:3,
+        name:"Full Sleeve Tshirts",
+        category:"women",
+        image:p3_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:4,
+        name:"Full Sleeve Tshirts",
+        category:"women",
+        image:p4_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:5,
+        name:"Full Sleeve Tshirts",
+        category:"women",
+        image:p5_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:6,
+        name:"Full Sleeve Tshirts",
+        category:"women",
+        image:p6_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:7,
+        name:"Full Sleeve Tshirts",
+        category:"women",
+        image:p7_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:8,
+        name:"Full Sleeve Tshirts",
+        category:"women",
+        image:p8_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:9,
+        name:"Full Sleeve Tshirts",
+        category:"women",
+        image:p9_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:10,
+        name:"Full Sleeve Tshirts",
+        category:"Kid",
+        image:p10_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:11,
+        name:"Full Sleeve Tshirts",
+        category:"Kid",
+        image:p11_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:12,
+        name:"Full Sleeve Tshirts",
+        category:"Kid",
+        image:p12_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:13,
+        name:"Full Sleeve Tshirts",
+        category:"Kid",
+        image:p13_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:14,
+        name:"Full Sleeve Tshirts",
+        category:"Kid",
+        image:p14_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },{
+        id:15,
+        name:"Full Sleeve Tshirts",
+        category:"Kid",
+        image:p15_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:16,
+        name:"Full Sleeve Tshirts",
+        category:"Kid",
+        image:p16_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:17,
+        name:"Full Sleeve Tshirts",
+        category:"women",
+        image:p17_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:18,
+        name:"Full Sleeve Tshirts",
+        category:"women",
+        image:p18_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:19,
+        name:"Full Sleeve Tshirts",
+        category:"women",
+        image:p19_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+    {
+        id:20,
+        name:"Full Sleeve Tshirts",
+        category:"women",
+        image:p20_img,
+        new_price: 50.0,
+        old_price: 100.0,
+    },
+
+]
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import './Navbar.css'  
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
+
 const Navbar = ()=>{
     return (
-        <div>
+        <div className='navbar'>
+            <div className='nav-logo'>
+                <img src={logo} alt="" width={"1000%"}/>
             
+            <p>SHOPPER</p>
+            </div>
+            <ul className="nav-menu">
+                <li>Shop</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>Kids</li>
+            </ul>
+            <div className="nav-login-cart">
+                <button>Login</button>
+                <img src={cart_icon} alt="" width={"10%"}/>
+                {/* <img src={cart_icon} alt="" /> */}
+                <div className="nav-cart-count">0</div>
+            </div>
         </div>
+            
     );
 }
 
